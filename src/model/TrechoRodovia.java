f95ee168dbd28410145774b73bcec4548c3ec01f
Massayoshii <Massayoshibando@gmail.com>
2026-04-25 13:17:10 -0300

    Metodo registrar crescimento criado e excecoes implementadas

--- src/model/TrechoRodovia.java
+++ src/model/TrechoRodovia.java
@@ -9,7 +9,7 @@ public class TrechoRodovia {
       this.setQuilometroInicial(quilometroInicial);
       this.setQuilometroFinal(quilometroFinal);
       this.setNivelVegetacaoCm(nivelVegetacaoCm);
-      System.out.println("Trecho rodovia: Quilometro inicial :"+this.quilometroInicial + "km | Quilometro final:"+ this.quilometroFinal + "km | Nivel de vegetacao:"+nivelVegetacaoCm + "cm");
+        System.out.println("Trecho rodovia: Km inicial:"+ this.quilometroInicial + "km | Quilometro final:"+ this.quilometroFinal + "km | Nivel de vegetacao:"+ this.nivelVegetacaoCm + "cm");
     }
 
     public double getQuilometroInicial() {
@@ -24,19 +24,30 @@ public class TrechoRodovia {
         return nivelVegetacaoCm;
     }
 
+
+    public void registrarCrescimento(double taxa){
+        if(taxa>0){
+            nivelVegetacaoCm += taxa;
+            System.out.println("Nivel de vegetacao aumentou para "+ this.nivelVegetacaoCm);
+        }else {
+            throw new IllegalArgumentException("A taxa nao pode ser 0 ou menor");
+        }
+
+    }
+
     public void setQuilometroInicial(double quilometroInicial) {
         if(quilometroInicial >= 0){
             this.quilometroInicial = quilometroInicial;
         }else {
-            System.out.println("Valor para quilometroInicial invalido");
+            throw new IllegalArgumentException("Quilometro inicial nao pode ser negativo");
         }
     }
 
     public void setQuilometroFinal(double quilometroFinal) {
-        if(quilometroFinal >= 0 && quilometroFinal > quilometroInicial){
+        if(quilometroFinal > quilometroInicial){
             this.quilometroFinal = quilometroFinal;
         }else {
-            System.out.println("Valor para quilometroFinal invalido");
+            throw new IllegalArgumentException("Quilometro final nao pode ser maior que o quilometro inicial");
         }
     }
 
@@ -44,7 +55,7 @@ public class TrechoRodovia {
         if(nivelVegetacaoCm >= 0){
             this.nivelVegetacaoCm = nivelVegetacaoCm;
         }else {
-            System.out.println("Valor para nivelVegetacaoCm invalido");
+            throw new IllegalArgumentException("Nivel de vegetacao nao pode ser negativo");
         }
     }
 
