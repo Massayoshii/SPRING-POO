450556d3df8ae2e5fb646e8022d245c2364258d5
Massayoshii <Massayoshibando@gmail.com>
2026-04-25 10:58:24 -0300

    Getters e Setters criados

--- src/model/TrechoRodovia.java
+++ src/model/TrechoRodovia.java
@@ -1,10 +1,48 @@
 package model;
 
 public class TrechoRodovia {
-    private double quilometroInicial;
-    private double quilometroFinal;
-    private double nivelVegetacao;
+    private double quilometroInicialCentimetros;
+    private double quilometroFinalCentimetros;
+    private double nivelVegetacaoCentimetros;
 
+
+
+
+    public double getQuilometroInicialCentimetros() {
+        return quilometroInicialCentimetros;
+    }
+
+    public double getQuilometroFinalCentimetros() {
+        return quilometroFinalCentimetros;
+    }
+
+    public double getNivelVegetacaoCentimetros() {
+        return nivelVegetacaoCentimetros;
+    }
+
+    public void setQuilometroInicialCentimetros(double quilometroInicialCentimetros) {
+        if(quilometroInicialCentimetros >= 0){
+            this.quilometroInicialCentimetros = quilometroInicialCentimetros;
+        }else {
+            System.out.println("Valor para quilometroInicialCentimetros invalido");
+        }
+    }
+
+    public void setQuilometroFinalCentimetros(double quilometroFinalCentimetros) {
+        if(quilometroFinalCentimetros >= 0){
+            this.quilometroFinalCentimetros = quilometroFinalCentimetros;
+        }else {
+            System.out.println("Valor para quilometroFinalCentimetros invalido");
+        }
+    }
+
+    public void setNivelVegetacaoCentimetros(double nivelVegetacaoCentimetros) {
+        if(nivelVegetacaoCentimetros >= 0){
+            this.nivelVegetacaoCentimetros = nivelVegetacaoCentimetros;
+        }else {
+            System.out.println("Valor para nivelVegetacaoCentimetros invalido");
+        }
+    }
 
 
 }
